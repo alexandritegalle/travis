403f52227ecc29e324aeee72d9c4844cd9291e5d
Pawan Gamage <alexandritegalle@gmail.com>
2020-02-17 18:47:15 +1100

    two

---
+++ build/precache-manifest.905177e279c64f278051f4afc7b7838d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ba9967a808bd180d414a7125ec95928",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08e6ed0ee2a3b7219820",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2e0db371cb2f4c4bf37e",
+    "url": "/static/js/2.d6dc25bb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.d6dc25bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08e6ed0ee2a3b7219820",
+    "url": "/static/js/main.a6776d63.chunk.js"
+  },
+  {
+    "revision": "e105983e5bef9c430ed2",
+    "url": "/static/js/runtime-main.e0dd47f9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
