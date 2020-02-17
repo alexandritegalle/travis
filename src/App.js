97bd1a89c0cccf4ece2886546bb37fdd2635c9cc
Pawan Gamage <alexandritegalle@gmail.com>
2020-02-17 18:03:31 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Pawan Gamage APP
+          Pawan Gamage AP
         </p>
         <a
           className="App-link"
